c4bb8a5d45075975f44adb7fde0c2deb6302d38a
Diego Sanchez <diego.sanchez@corp.badoo.com>
2015-07-03 23:51:39 +0100

    Adds supports for carthage

---
+++ BMASlidersFramework/BMASliders.h
@@ -0,0 +1,41 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2015-present Badoo Trading Limited.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+@import UIKit;
+
+//! Project version number for BMASlidersFramework.
+FOUNDATION_EXPORT double BMASlidersFrameworkVersionNumber;
+
+//! Project version string for BMASlidersFramework.
+FOUNDATION_EXPORT const unsigned char BMASlidersFrameworkVersionString[];
+
+#import <BMASliders/BMALabeledRangeSlider.h>
+#import <BMASliders/BMALabeledSlider.h>
+#import <BMASliders/BMALabeledSliderConfiguring.h>
+#import <BMASliders/BMARangeFormatter.h>
+#import <BMASliders/BMARangeSlider.h>
+#import <BMASliders/BMASlider.h>
+#import <BMASliders/BMASliderLiveRenderingStyle.h>
+#import <BMASliders/BMASliderReusableXibControl.h>
+#import <BMASliders/BMASliderStyling.h>
